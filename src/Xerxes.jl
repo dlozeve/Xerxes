8eed5e93709415495b8b33c4b2f72c34af096762
Dimitri Lozeve <dimitri@lozeve.com>
2020-09-13 22:05:49 +0200

    Implement the primal iteration of the revised simplex algorithm

--- src/Xerxes.jl
+++ src/Xerxes.jl
@@ -1,5 +1,92 @@
 module Xerxes
 
-# Write your package code here.
+using Logging
+
+mutable struct Program
+    A::Matrix
+    b::Vector
+    c::Vector
+    nonbasic_vars::Vector
+    basic_vars::Vector
+    x::Vector # current primal solution
+    z::Vector # current dual solution
+end
+
+@enum Status Optimal Nonoptimal Unbounded Infeasible
+
+function primal_iterate!(prog::Program)
+    B = view(prog.A, :, prog.basic_vars)
+    N = view(prog.A, :, prog.nonbasic_vars)
+    @debug "Submatrices" B N
+
+    # Check for optimality
+    if all(prog.z[prog.nonbasic_vars] .>= 0)
+        @info "Program is optimal"
+        return Optimal
+    end
+    @debug "z has negative values, therefore the program is not optimal"
+
+    # Select entering variable
+    j = argmin(prog.z)
+    if prog.z[j] >= 0
+        @debug "No entering variable"
+        return Infeasible
+    end
+    @debug "Entering variable" j
+
+    # Compute primal step direction
+    ej = zeros(length(prog.x))
+    ej[j] += 1
+    primal_step = B \ (N * ej[prog.nonbasic_vars])
+    @debug "Primal step direction" primal_step
+
+    # Compute primal step length
+    inv_primal_step_length = -Inf
+    i = 0
+    for k in eachindex(prog.basic_vars)
+        if primal_step[k] == 0 && prog.x[prog.basic_vars[k]] == 0
+            t = 0
+        else
+            t = primal_step[k] / prog.x[prog.basic_vars[k]]
+        end
+        if inv_primal_step_length < t
+            inv_primal_step_length = t
+            i = prog.basic_vars[k]
+        end
+    end
+    if inv_primal_step_length <= 0
+        return Unbounded
+    end
+    primal_step_length = 1 / inv_primal_step_length
+    @debug "Primal step length" primal_step_length
+    primal_step *= primal_step_length
+
+    # Select leaving variable (already done, i above)
+    @debug "Leaving variable" i
+
+    # Compute dual step direction
+    ei = zeros(length(prog.z))
+    ei[i] += 1
+    dual_step = - N' * (B' \ ei[prog.basic_vars])
+    @debug "Dual step direction" dual_step
+
+    # Compute dual step length
+    idx = indexin(j, prog.nonbasic_vars)[1]
+    dual_step_length = prog.z[j] / dual_step[idx]
+    @debug "Dual step length" dual_step_length
+    dual_step *= dual_step_length
+
+    # Update current dual and primal solutions
+    prog.x[j] = primal_step_length
+    prog.x[prog.basic_vars] = prog.x[prog.basic_vars] - primal_step
+    prog.z[i] = dual_step_length
+    prog.z[prog.nonbasic_vars] = prog.z[prog.nonbasic_vars] - dual_step
+
+    # Update basis
+    prog.basic_vars = union(setdiff(prog.basic_vars, [i]), [j])
+    prog.nonbasic_vars = union(setdiff(prog.nonbasic_vars, [j]), [i])
+
+    return Nonoptimal
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,65 @@ using Xerxes
 using Test
 
 @testset "Xerxes.jl" begin
-    # Write your tests here.
+    prog = Xerxes.Program(
+        [
+            1.0 -1 1 0 0
+            2.0 -1 0 1 0
+            0    1 0 0 1
+        ],
+        [1.0, 3, 5],
+        [4.0, 3, 0, 0, 0],
+        [1, 2],
+        [3, 4, 5],
+        [0.0, 0, 1, 3, 5],
+        [-4.0, -3, 0, 0, 0]
+    )
+
+    status = Xerxes.primal_iterate!(prog)
+    @test status == Xerxes.Nonoptimal
+    @test issetequal(prog.basic_vars, [1, 4, 5])
+    @test issetequal(prog.nonbasic_vars, [3, 2])
+    @test prog.A[:, sort(prog.basic_vars)] ==
+        [ 1 0 0
+          2 1 0
+          0 0 1 ]
+    @test prog.A[:, sort(prog.nonbasic_vars)] ==
+        [ -1 1
+          -1 0
+           1 0 ]
+    @test prog.x[sort(prog.basic_vars)] == [1, 1, 5]
+    @test prog.z[sort(prog.nonbasic_vars)] == [-7, 4]
+
+    status = Xerxes.primal_iterate!(prog)
+    @test status == Xerxes.Nonoptimal
+    @test issetequal(prog.basic_vars, [1, 2, 5])
+    @test issetequal(prog.nonbasic_vars, [3, 4])
+    @test prog.A[:, sort(prog.basic_vars)] ==
+        [ 1 -1 0
+          2 -1 0
+          0  1 1 ]
+    @test prog.A[:, sort(prog.nonbasic_vars)] ==
+        [ 1 0
+          0 1
+          0 0 ]
+    @test prog.x[sort(prog.basic_vars)] == [2, 1, 4]
+    @test prog.z[sort(prog.nonbasic_vars)] == [-10, 7]
+
+    status = Xerxes.primal_iterate!(prog)
+    @test status == Xerxes.Nonoptimal
+    @test issetequal(prog.basic_vars, [1, 2, 3])
+    @test issetequal(prog.nonbasic_vars, [5, 4])
+    @test prog.A[:, sort(prog.basic_vars)] ==
+        [ 1 -1 1
+          2 -1 0
+          0  1 0 ]
+    @test prog.A[:, sort(prog.nonbasic_vars)] ==
+        [ 0 0
+          1 0
+          0 1 ]
+    @test prog.x[sort(prog.basic_vars)] == [4, 5, 2]
+    @test prog.z[sort(prog.nonbasic_vars)] == [2, 5]
+
+    status = Xerxes.primal_iterate!(prog)
+    @test status == Xerxes.Optimal
 end
